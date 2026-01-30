a0c869700b191de358955c5b255ea4897aa9c9b2
bishkekpochta312-lab <bishkek.pochta312@gmail.com>
2026-01-30 23:39:32 +0600

    update

--- GitHelp.txt.md
+++ GitHelp.txt.md
@@ -5,6 +5,8 @@ git init
 git remote add origin https://github.com/bishkekpochta312-lab/oop-63-2
 
 git status | состояние проекта
-git add.   | скопировать все в буфер обмена
+git add .   | скопировать все в буфер обмена
 git commit -m "lesson1"
+git commit -m "update lesson1"
 git branch
+git push origin master
--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,28 @@
-print("Mir")
+class Hero:
+
+    # Конструктор класса
+    def __init__(self, nick, hp, lvl):
+        # Атрибуты класса
+        self.nick = nick
+        self.hero = hp
+        self.lvl = lvl
+
+    # методы класса
+    def action(self):
+        return f"{self.nick} base action activate!!"
+
+# Объект/Экземпляр класса
+kirito = Hero("Kirito", 1000, 100)
+asuna = Hero("Asuna", 1100, 101)
+my_int = 123
+# my_int.
+my_str = "123"
+# my_str.
+print(type(my_int))
+print(type(kirito))
+
+# print(kirito.action())
+# print(asuna.action())
+
+# def some():
+#     pass
